75baa2cfecda9a13f6a25e70ac9cc6ce9aa0eeee
Dieu <lndieu2508@gmail.com>
2019-12-06 21:27:15 +0700

    for vu

---
+++ KiemTra/DAL/Entity/ChiTietDanhBa.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KiemTra.DAL.Entity
+{
+    class ChiTietDanhBa
+    {
+        [Key]
+        public string Tengoi { get; set; }
+        public string Diachi { get; set; }
+        public string Email { get; set; }
+        public string SDT { get; set; }
+        public string TenNhom { get; set; }
+        [ForeignKey("TenNhom")]
+        public virtual NhómDanhBa NhómDanhBa { get; set; }
+        public static List<ChiTietDanhBa> GetListFromFile(string pathData,string tenNhom)
+        {
+            var arrayLines = File.ReadAllLines(pathData);
+            List<ChiTietDanhBa> ketQua = new List<ChiTietDanhBa>();
+            foreach (var line in arrayLines)
+            {
+                var lsValue = line.Split(new char[] { '#' });
+
+                var chitietdanhba = new ChiTietDanhBa
+                {
+                    Tengoi = lsValue[1],
+                    Diachi = lsValue[2],
+                    Email = lsValue[3],
+                    SDT = lsValue[4],
+                    TenNhom = lsValue[5],
+                };
+                if (chitietdanhba.TenNhom == tenNhom )
+                    ketQua.Add(chitietdanhba);
+            }
+            return ketQua;
+        }
+        public static List<ChiTietDanhBa> GetListFromDB(string Tennhom)
+        {
+            return new KiemTraDBContext().ChiTietDanhBaDbSet.Where(e => e.TenNhom == Tennhom).ToList();
+        }
+        public static void Remove(ChiTietDanhBa ten)
+        {
+            var db = new KiemTraDBContext();
+            var ojbSV = db.ChiTietDanhBaDbSet.Where(e => e.Tengoi == ten.Tengoi).FirstOrDefault();
+            if (ojbSV != null)
+            {
+                db.ChiTietDanhBaDbSet.Remove(ojbSV);
+
+            }
+            db.SaveChanges();
+        }
+        public static void Add(ChiTietDanhBa nhom)
+        {
+            var db = new KiemTraDBContext();
+            db.ChiTietDanhBaDbSet.Add(nhom);
+            db.SaveChanges();
+
+        }
+    }
+}
---
+++ KiemTra/DAL/Entity/NhómDanhBa.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KiemTra.DAL.Entity
+{
+    class NhómDanhBa
+    {   [Key]
+        public string TenNhom { get; set; }
+        
+        public static List<NhómDanhBa> GetListFromFile(string pathData)
+        {
+            var arrayLines = File.ReadAllLines(pathData);
+            List<NhómDanhBa> ketQua = new List<NhómDanhBa>();
+            foreach (var line in arrayLines)
+            {
+                var lsValue = line.Split(new char[] { '#' });
+                var nhomdanhba = new NhómDanhBa
+                {
+                    TenNhom=lsValue[1],
+                };
+                
+                    ketQua.Add(nhomdanhba);
+            }
+            return ketQua;
+        }
+        public static void Remove(NhómDanhBa nhom)
+        {
+            var db = new KiemTraDBContext();
+            var ojbSV = db.NhomDanhBaDbSet.Where(e => e.TenNhom == nhom.TenNhom).FirstOrDefault();
+            if (ojbSV != null)
+            {
+                db.NhomDanhBaDbSet.Remove(ojbSV);
+               
+            }
+            db.SaveChanges();
+        }
+        public static List<NhómDanhBa> GetListFromDB()
+        {
+            return new KiemTraDBContext().NhomDanhBaDbSet.ToList();
+        }
+        public static void Add(NhómDanhBa nhom)
+        {
+            var db = new KiemTraDBContext();
+            db.NhomDanhBaDbSet.Add(nhom);
+            db.SaveChanges();
+
+        }
+    }
+}
